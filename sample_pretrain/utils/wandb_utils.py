70cf7af9c949b459db512239ad69fa008b66a017
Bartłomiej Cupiał <bartlomiej.cupial@gmail.com>
2023-12-27 18:36:52 +0100

    fix wandb for single gpu

--- sample_pretrain/utils/wandb_utils.py
+++ sample_pretrain/utils/wandb_utils.py
@@ -2,6 +2,7 @@ from datetime import datetime
 
 import torch.distributed as dist
 
+from sample_pretrain.algo.utils.context import global_ddp_mode
 from sample_pretrain.utils.utils import log, retry
 
 
@@ -17,7 +18,10 @@ def init_wandb(cfg):
 
     if "wandb_unique_id" not in cfg:
         # if we're going to restart the experiment, this will be saved to a json file
-        cfg.wandb_unique_id = f'{cfg.experiment}_{datetime.now().strftime(f"%Y%m%d_%H%M%S_%f")}_{dist.get_rank()}'
+        cfg.wandb_unique_id = f'{cfg.experiment}_{datetime.now().strftime(f"%Y%m%d_%H%M%S_%f")}'
+
+        if global_ddp_mode():
+            cfg.wandb_unique_id += f"_{dist.get_rank()}"
 
     wandb_unique_id = cfg.wandb_unique_id
     wandb_group = cfg.env if cfg.wandb_group is None else cfg.wandb_group
